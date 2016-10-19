e47a9dcec6915f1a9edd6b84fc5c08a278a7cdb6
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-10-18 23:02:08 -0500

    first commit

---
+++ client.js
@@ -0,0 +1,9 @@
+var app = angular.module('dApp', []);
+app.controller('dctrl', function($scope) {
+  $scope.classColor = "Enter Color Name";
+});
+
+
+app.controller('zctrl', function($scope) {
+  $scope.classFont = "Enter Font Name";
+});
